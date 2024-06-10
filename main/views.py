777ba80676be7507e4d6ae7060a5fa4c8e8ba2e0
qumi3121 <bahz1997@yandex.ru>
2024-06-10 15:43:13 +0400

    First commit in diplom

---
+++ main/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    context = {
+        'title': 'Hello World',
+        'content': 'This is main page'
+    }
+    return render(request, 'main/index.html', context)
+
+
+def about(request):
+    return HttpResponse("About page")
