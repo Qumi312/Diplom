685b1bff128a7b071e6a664c4363e264e82adef8
Ivan <bahz1997@gmail.com>
2024-06-13 15:39:09 +0400

    Added fixtures

--- main/views.py
+++ main/views.py
@@ -1,12 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from goods.models import Categories
 
 # Create your views here.
 def index(request):
+
+    categories = Categories.objects.all()
+
     context = {
         'title': 'Home - Главная',
         'content': 'Магазин мебели HOME',
+        'categories': categories,
     }
     return render(request, 'main/index.html', context)
 
@@ -18,3 +23,4 @@ def about(request):
         'text_on_page': 'Текст о том какой наш товар неплохой',
     }
     return render(request, 'main/about.html', context)
+
