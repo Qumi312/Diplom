88e243a5fe092be116e41665fd3cd9dd19b6aa18
Ivan <bahz1997@gmail.com>
2024-06-18 02:15:06 +0400

    All cart templates were included

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     
     'debug_toolbar',
     'users',
+    'carts',
     'main.apps.MainConfig',
     'goods.apps.GoodsConfig',
     
@@ -147,3 +148,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
+LOGIN_URL = '/user/login/'
